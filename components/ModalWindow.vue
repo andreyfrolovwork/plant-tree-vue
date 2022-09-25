13ac2155b6716e81852d9de726b9fa8b2f619f9c
Andrey <iobox420@gmail.com>
2022-09-25 19:58:42 +0500

    feat: hide screen after see and save state in localStorage

---
+++ components/ModalWindow.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+section.modal
+  div.wrapper
+    slot
+</template>
+
+<script>
+export default {
+  name: "ModalWindow",
+  props:['isOpenModal'],
+  methods:{
+
+  }
+};
+</script>
+
+<style scoped>
+.wrapper {
+  display: none;
+  position: fixed;
+  opacity: 0.5;
+
+}
+</style>
